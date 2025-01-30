aad07572102791a4a7efe946b367ae2f264bfa1d
fayaass <muhammedfayas59504@gmail.com>
2025-01-30 11:34:45 +0530

    add

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/software-9am/Desktop/port/Portfolio/env"
+set -gx VIRTUAL_ENV "/home/software-9am/Desktop/portt/Portfolio/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
